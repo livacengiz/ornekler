4c561289ac050b52384e7ef98f80badb05322a33
Liva <ahmetlivacengiz@gmail.com>
2015-02-01 15:02:58 +0200

    Update dict-and-count.py
    text içerisinde bulunan kelimeleri uppercase ederek daha verimli sonuç getirmesini sağlıyor.

--- dict-and-count.py
+++ dict-and-count.py
@@ -3,7 +3,7 @@ dizi = {}
 
 for kelime in kelimeler:
     if (len(kelime) > 3):
-        dizi[kelime] = kelimeler.count(kelime)
+        dizi[kelime.upper()] = kelimeler.count(kelime)
 
 for (key, val) in dizi.items():
     print "%15s kelime : %s" %(key, "*" * val)
